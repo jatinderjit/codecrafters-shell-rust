c06490b76637f91e1e6718426ade593e06b9b890
Jatinderjit Singh <jatinderjit89@gmail.com>
2024-12-06 23:56:35 +0530

    Refactor REPL

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,5 @@
-#[allow(unused_imports)]
-use std::io::{self, Write};
+use codecrafters_shell::run;
 
 fn main() {
-    loop {
-        repl();
-    }
-}
-
-fn repl() {
-    print!("$ ");
-    io::stdout().flush().unwrap();
-
-    // Wait for user input
-    let stdin = io::stdin();
-    let mut input = String::new();
-    stdin.read_line(&mut input).unwrap();
-
-    let command = input.trim_matches(['\r', '\n']);
-    process(command);
-}
-
-fn process(command: &str) {
-    let command_with_args = command.splitn(2, ' ').collect::<Vec<_>>();
-
-    match command_with_args[0] {
-        "exit" => exit(&command_with_args[1..]),
-        "echo" => println!("{}", command_with_args.get(1).unwrap_or(&"")),
-        _ => println!("{command}: command not found"),
-    };
-}
-
-fn exit(args: &[&str]) -> ! {
-    if args.len() == 1 {
-        let code = args[0].parse::<u8>().unwrap_or(1);
-        std::process::exit(code as i32);
-    }
-    std::process::exit(0);
+    run();
 }
---
+++ src/repl.rs
@@ -0,0 +1,40 @@
+use std::io::{self, Write};
+
+pub fn run() {
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
+
+        // Wait for user input
+        let stdin = io::stdin();
+        let mut input = String::new();
+        stdin.read_line(&mut input).unwrap();
+
+        let input = input.trim_matches(['\r', '\n']);
+        process(input);
+    }
+}
+
+fn invalid_command(command: &str) {
+    println!("{command}: command not found");
+}
+
+fn process(input: &str) {
+    let command_with_args = input.splitn(2, ' ').collect::<Vec<_>>();
+    let command = command_with_args[0];
+    let args = command_with_args.get(1);
+
+    match command {
+        "exit" => exit(command_with_args.get(1).copied()),
+        "echo" => println!("{}", args.unwrap_or(&"")),
+        _ => invalid_command(command),
+    };
+}
+
+fn exit(args: Option<&str>) -> ! {
+    let code = match args {
+        Some(code) => code.parse().unwrap_or(1),
+        None => 0,
+    };
+    std::process::exit(code);
+}
