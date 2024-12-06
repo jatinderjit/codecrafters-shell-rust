2050ae433afe72b5a3d60202df1a7fb161fe6199
Jatinderjit Singh <jatinderjit89@gmail.com>
2024-12-07 04:52:44 +0530

    Handle arguments in single quotes

--- src/builtins.rs
+++ src/builtins.rs
@@ -37,12 +37,12 @@ impl FromStr for Builtin {
 }
 
 impl Builtin {
-    pub(crate) fn execute(&self, args: Option<&str>) {
+    pub(crate) fn execute(&self, args: Vec<String>) {
         use Builtin::*;
 
         match self {
             Cd => process_cd(args),
-            Echo => println!("{}", args.unwrap_or("")),
+            Echo => println!("{}", args.join(" ")),
             Exit => process_exit(args),
             Pwd => println!("{}", env::current_dir().unwrap().display()),
             Type => process_type(args),
@@ -50,9 +50,12 @@ impl Builtin {
     }
 }
 
-#[allow(deprecated)]
-fn process_cd(args: Option<&str>) {
-    let mut path = match args {
+fn process_cd(args: Vec<String>) {
+    if args.len() > 1 {
+        println!("cd: too many arguments");
+        return;
+    }
+    let mut path = match args.first() {
         Some(path) => expand_home(path),
         None => home_dir(),
     };
@@ -62,26 +65,28 @@ fn process_cd(args: Option<&str>) {
     if path.is_dir() {
         set_current_dir(path).unwrap();
     } else {
-        println!("cd: {}: No such file or directory", args.unwrap_or(""));
+        println!("cd: {}: No such file or directory", args[0]);
     }
 }
 
-fn process_exit(args: Option<&str>) -> ! {
-    let code = match args {
+fn process_exit(args: Vec<String>) {
+    if args.len() > 1 {
+        println!("exit: too many arguments");
+        return;
+    }
+    let code = match args.first() {
         Some(code) => code.parse().unwrap_or(1),
         None => 0,
     };
     std::process::exit(code);
 }
 
-fn process_type(args: Option<&str>) {
-    let subcommand = match args {
-        Some(subcommand) => subcommand,
-        None => return,
-    };
-    match subcommand.parse::<Executable>() {
-        Ok(Executable::Builtin(_)) => println!("{subcommand} is a shell builtin"),
-        Ok(Executable::Binary(path)) => println!("{subcommand} is {}", path.display()),
-        Err(_) => println!("{subcommand}: not found"),
-    };
+fn process_type(args: Vec<String>) {
+    for subcommand in args {
+        match subcommand.parse::<Executable>() {
+            Ok(Executable::Builtin(_)) => println!("{subcommand} is a shell builtin"),
+            Ok(Executable::Binary(path)) => println!("{subcommand} is {}", path.display()),
+            Err(_) => println!("{subcommand}: not found"),
+        };
+    }
 }
--- src/repl.rs
+++ src/repl.rs
@@ -1,7 +1,9 @@
 use std::{
     io::{self, Write},
+    iter::Peekable,
     path::PathBuf,
     process::Command,
+    str::Chars,
 };
 
 use crate::executables::Executable;
@@ -16,15 +18,22 @@ pub fn run() {
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
 
-        let input = input.trim_matches(['\r', '\n']);
-        process(input);
+        execute(input.trim());
     }
 }
 
-fn process(input: &str) {
-    let command_with_args = input.splitn(2, ' ').collect::<Vec<_>>();
-    let command = command_with_args[0];
-    let args = command_with_args.get(1).copied();
+fn execute(input: &str) {
+    let (command, args) = match parse_command(input) {
+        Ok((command, args)) => (command, args),
+        Err(err) => {
+            println!("{err}");
+            return;
+        }
+    };
+    let command = match command {
+        Some(command) if !command.is_empty() => command,
+        _ => return,
+    };
 
     let executable = command.parse::<Executable>();
     match executable {
@@ -34,10 +43,70 @@ fn process(input: &str) {
     };
 }
 
-fn execute_binary(path: PathBuf, args: Option<&str>) {
+fn execute_binary(path: PathBuf, args: Vec<String>) {
     let mut command = Command::new(path);
-    if let Some(args) = args {
-        command.arg(args);
-    }
+    command.args(args);
     command.status().expect("Failed to execute process");
+}
+
+fn parse_command(input: &str) -> Result<(Option<&str>, Vec<String>), &'static str> {
+    let mut command_with_args = input.splitn(2, ' ');
+    let command = command_with_args.next();
+    let args = match command_with_args.next() {
+        Some(input) => match parse_args(input) {
+            Ok(args) => args,
+            Err(err) => return Err(err),
+        },
+        None => Vec::new(),
+    };
+    Ok((command, args))
+}
+
+fn parse_args(input: &str) -> Result<Vec<String>, &'static str> {
+    let mut args = vec![];
+    let mut it = input.chars().peekable();
+    skip_whitespace(&mut it);
+    while it.peek().is_some() {
+        match parse_arg(&mut it) {
+            Ok(arg) => args.push(arg),
+            Err(err) => return Err(err),
+        }
+        skip_whitespace(&mut it);
+    }
+    Ok(args)
+}
+
+fn parse_arg(it: &mut Peekable<Chars>) -> Result<String, &'static str> {
+    let mut arg = vec![];
+    let mut single_quote = it.peek() == Some(&'\'');
+    if single_quote {
+        it.next();
+    }
+    for ch in it {
+        if ch == '\'' {
+            if !single_quote {
+                return Err("Invalid arguments");
+            }
+            single_quote = false;
+            break;
+        }
+        if !single_quote && (ch == ' ' || ch == '\t') {
+            break;
+        }
+        arg.push(ch);
+    }
+    if single_quote {
+        return Err("Invalid arguments");
+    }
+    Ok(arg.into_iter().collect())
+}
+
+fn skip_whitespace(it: &mut Peekable<Chars>) {
+    while let Some(ch) = it.peek() {
+        if *ch == ' ' || *ch == '\t' {
+            it.next();
+        } else {
+            break;
+        }
+    }
 }
